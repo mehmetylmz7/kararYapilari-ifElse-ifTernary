0c24e891441f2a575a93b135bf161099115d38ad
Mehmet Yılmaz <109681167+mehmetylmz7@users.noreply.github.com>
2022-08-22 08:15:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kararYapilari_ifElse_ifTernary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time<11)
+                Console.WriteLine("Günaydın!");
+            else if(time<=18)
+                Console.WriteLine("iyi günler!");
+            else
+                Console.WriteLine("iyi geceler!");
+
+
+
+            string sonuc = time <= 18 ? "iyi günler!" : "iyi geceler!";
+
+            Console.WriteLine(sonuc);
+
+            sonuc = time >= 6 && time < 11 ? "gunaydın!" : time <= 18 ? "iyi günler!" : "iyi geceler!";
+
+            Console.WriteLine(sonuc);
+            
+            
+            
+            
+            
+            
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
